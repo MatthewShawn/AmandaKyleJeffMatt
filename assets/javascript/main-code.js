088b225a96e6d40a608d0a737995194833bb9865
Jeff Westcott <jeffwestcott@Jeffs-MacBook-Pro.local>
2019-04-30 20:18:26 -0600

    Places function sorta

--- assets/javascript/main-code.js
+++ assets/javascript/main-code.js
@@ -8,7 +8,9 @@ var config = {
     messagingSenderId: "832785980119"
 };
 firebase.initializeApp(config);
-
+var start = "";
+//var end = "Conifer, CO";
+var end = "";
 var database = firebase.database();
 
 
@@ -163,6 +165,9 @@ var remove = function (e) {
     var key1 = $(this).data("data-key");
     console.log(key1);
     database.ref(key1).remove();
+    $(".food-table").hide();
+    
+    $(".map-div").show();
 }
 
 $(document).on("click", ".claim", remove);
@@ -188,6 +193,11 @@ database.ref().on("child_removed", function (snapshot) {
     pickupDateSelected = snapshot.dateAvail;
     pickupTimeSelected = snapshot.pickupTime;
 
+    console.log(addressSelected);
+   
+    start = "Denver, CO";
+    end = addressSelected;
+
 
 })
 
@@ -195,19 +205,9 @@ database.ref().on("child_removed", function (snapshot) {
 
 
 
-var start = "Denver, CO";
-//var end = "Conifer, CO";
-var end = "Conifer, CO";
-
-$(document).on("click", ".claim", function () {
-    //Claim is a dynamic button, so we must used $(document)
-    $(".food-table").hide();
-    start = "Denver, CO";
-    end = "Conifer, CO";
-    $(".map-div").show();
 
 
-});
+
 
 $("#exit-btn").on("click", function () {
     $(".map-div").hide();
@@ -276,4 +276,67 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
             window.alert('Directions request failed due to ' + status);
         }
     });
+}
+
+var placeSearch, autocomplete;
+
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search predictions to
+  // geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('autocomplete'), {types: ['geocode']});
+
+  // Avoid paying for data that you don't need by restricting the set of
+  // place fields that are returned to just the address components.
+  autocomplete.setFields(['address_component']);
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log(place);
+
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle(
+          {center: geolocation, radius: position.coords.accuracy});
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
 }
