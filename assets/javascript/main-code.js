9bf1273e9d01d3f07ea3edc59639233d7d888a79
Amanda Dovel <amandacdovel@gmail.com>
2019-05-01 19:42:29 -0600

    fixed button color, added header to table page

--- assets/javascript/main-code.js
+++ assets/javascript/main-code.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
   $("body").addClass("test");
   $('.modal').modal();
   $('.dropdown-trigger').dropdown();
+  $('select').formSelect();
 })
 
 // Home page ---> donor-register
@@ -39,14 +40,14 @@ $("#find-button").on("click", function () {
 })
 
 // (KYLE) Take Me Home Btn ---> Homepage
-$("#home-button").on("click", function(){
+$("#home-button").on("click", function () {
 
   $("#welcome-page").removeClass("hide");
   $("#request-received").addClass("hide");
 })
 
 // (KYLE) Donate Again ---> Donor Form
-$("#again-button").on("click", function(){
+$("#again-button").on("click", function () {
 
   $(".donor-form").removeClass("hide");
   $("#request-received").addClass("hide");
@@ -62,6 +63,7 @@ $("#add-recipient-btn").on("click", function () {
   event.preventDefault();
   $(".pickup-form").addClass("hide");
   $("#food-table").removeClass("hide");
+  $(".table-title").removeClass("hide");
 })
 
 // donor register ---> request received
@@ -162,7 +164,7 @@ database.ref().on("child_added", function (childSnapshot) {
     newButton.attr("data-key", key);
     newButton.text("Claim!")
     newButton.data("data-key", key);
-    newButton.addClass("claim btn btn-primary " + key);
+    newButton.addClass("claim waves-effect waves-light btn-large " + key);
     newRow.append(newButton);
 
 
@@ -184,22 +186,22 @@ database.ref().on("child_added", function (childSnapshot) {
 //   $(".map-div").removeClass("hide");
 // }
 
-var remove = function remove(e){
-    console.log("I work");
-    var key = $(this).data("data-key");
-    console.log(key);
-    // call db update child (key)
-   var parent =  $("."+key).parent('tr');
-   console.log(database.ref(key))
+var remove = function remove(e) {
+  console.log("I work");
+  var key = $(this).data("data-key");
+  console.log(key);
+  // call db update child (key)
+  var parent = $("." + key).parent('tr');
+  console.log(database.ref(key))
 
-//    How to access specific child elements in firebase????????
-//    database.ref(key).claimed.set("true");
-   console.log(parent);
-    parent.remove()
-    start = "Denver, CO";
-    end = "Conifer, CO";
-    $(".map-div").removeClass("hide");
-    initMap();
+  //    How to access specific child elements in firebase????????
+  //    database.ref(key).claimed.set("true");
+  console.log(parent);
+  parent.remove()
+  start = "Denver, CO";
+  end = "Conifer, CO";
+  $(".map-div").removeClass("hide");
+  initMap();
 
 }
 
@@ -240,132 +242,132 @@ database.ref().on("child_removed", function (snapshot) {
 
 // The button that takes you back to the home page 
 $("#exit-btn").on("click", function () {
-    $(".map-div").addClass("hide");
-    $(".food-table").addClass("hide");
-    $(".donor-form").addClass("hide");
-    $(".pickup-form").addClass("hide");
-    $("#request-received").addClass("hide");
-    $("#welcome-page").removeClass("hide");
-    //The empty statements guarantee that the map page is being re-written.
-    //If it fails to re-write, it will be blank the next time we try to 
-    //navigate to it.
-    $("#right-panel").empty();
-    $("#map").empty();
-  });
+  $(".map-div").addClass("hide");
+  $(".food-table").addClass("hide");
+  $(".donor-form").addClass("hide");
+  $(".pickup-form").addClass("hide");
+  $("#request-received").addClass("hide");
+  $("#welcome-page").removeClass("hide");
+  //The empty statements guarantee that the map page is being re-written.
+  //If it fails to re-write, it will be blank the next time we try to 
+  //navigate to it.
+  $("#right-panel").empty();
+  $("#map").empty();
+});
 
- // dropdown menu for organization type
- document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    //var instances = M.Dropdown.init(elems, options);
-  });
+// dropdown menu for organization type
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  //var instances = M.Dropdown.init(elems, options);
+});
 
 
 $(document).on("click", ".claim", function () {
   //Claim is a dynamic button, so we must used $(document)
-//   $(".food-table").addClass("hide");
-  
+  //   $(".food-table").addClass("hide");
+
 })
 
-  function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 7,
-      center: { lat: 41.85, lng: -87.65 }
+function initMap() {
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: { lat: 41.85, lng: -87.65 }
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+  //var control = document.getElementById('floating-panel');
+  //control.style.display = 'block';
+  //map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+  //var onChangeHandler = function() {
+
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  //};
+  //document.getElementById('start').addEventListener('change', onChangeHandler);
+  //document.getElementById('end').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  //var start = document.getElementById('start').value;
+  //var end = document.getElementById('end').value;
+  directionsService.route({
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  }, function (response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+var placeSearch, autocomplete;
+
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search predictions to
+  // geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('autocomplete'), { types: ['geocode'] });
+
+  // Avoid paying for data that you don't need by restricting the set of
+  // place fields that are returned to just the address components.
+  autocomplete.setFields(['address_component']);
+
+  // When the user selects an address from the drop-down, populate the
+  // address fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  console.log(place);
+
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
+  }
+
+  // Get each component of the address from the place details,
+  // and then fill-in the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
+    }
+  }
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle(
+        { center: geolocation, radius: position.coords.accuracy });
+      autocomplete.setBounds(circle.getBounds());
     });
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
-
-    //var control = document.getElementById('floating-panel');
-    //control.style.display = 'block';
-    //map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-
-    //var onChangeHandler = function() {
-
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-    //};
-    //document.getElementById('start').addEventListener('change', onChangeHandler);
-    //document.getElementById('end').addEventListener('change', onChangeHandler);
   }
-
-  function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    //var start = document.getElementById('start').value;
-    //var end = document.getElementById('end').value;
-    directionsService.route({
-      origin: start,
-      destination: end,
-      travelMode: 'DRIVING'
-    }, function (response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
-  }
-
-  var placeSearch, autocomplete;
-
-  var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
-  };
-
-  function initAutocomplete() {
-    // Create the autocomplete object, restricting the search predictions to
-    // geographical location types.
-    autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autocomplete'), { types: ['geocode'] });
-
-    // Avoid paying for data that you don't need by restricting the set of
-    // place fields that are returned to just the address components.
-    autocomplete.setFields(['address_component']);
-
-    // When the user selects an address from the drop-down, populate the
-    // address fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
-  }
-
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-    console.log(place);
-
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-      document.getElementById(component).disabled = false;
-    }
-
-    // Get each component of the address from the place details,
-    // and then fill-in the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
-    }
-  }
-
-  // Bias the autocomplete object to the user's geographical location,
-  // as supplied by the browser's 'navigator.geolocation' object.
-  function geolocate() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var geolocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        var circle = new google.maps.Circle(
-          { center: geolocation, radius: position.coords.accuracy });
-        autocomplete.setBounds(circle.getBounds());
-      });
-    }
-  }
+}
 
 
 
